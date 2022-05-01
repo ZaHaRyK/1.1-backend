de8bd58b1525a5d02a08e8a64d989ced4fbb8b94
ZaHaRyK <104730836+ZaHaRyK@users.noreply.github.com>
2022-05-01 21:38:17 +0300

    Update R1.js

--- R1.js
+++ R1.js
@@ -1,7 +1,8 @@
-let otsiv = new Reviews("296436","Oleg","good product","10/10")
+
+let otsiv = new Reviews("Oleg","good product","10/10")
 let reviews = otsiv
-function Product(id,name,description,price,brand,sizes,activeSize,quantity) {
-  this.id = id  ;
+function Product(name,description,price,brand,sizes,activeSize,quantity) {
+  this.id = Math.floor(Math.random() * 100) + 1  ;
   this.name = name;
   this.description = description;
   this.price = price;
@@ -14,10 +15,10 @@ function Product(id,name,description,price,brand,sizes,activeSize,quantity) {
   this.images = ["im1","im2","im3","im4","im5"]
 }
 
-let lold = new Product("645392","назва","краткий опис","ціна","бренд","розмір","активний розмір","продуктов в запасі");
+let lold = new Product("назва","краткий опис","ціна","бренд","розмір","активний розмір","продуктов в запасі");
 
-function Reviews (id,author,comment,rating) {
-  this.id = id;
+function Reviews (author,comment,rating) {
+  this.id = Math.floor(Math.random() * 100) + 1;
   this.date = new Date().toUTCString();
   this.author = author
   this.comment = comment
